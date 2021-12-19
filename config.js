0bf18f5d74bdb444d01ae89148dce47cdec8fc90
Bronco <heybronco@gmail.com>
2021-12-19 19:32:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 let obj = JSON.parse($response.body || '{}');
 
-if (obj.data[0]) {
-	obj.data[0].vip_split.is_filter_ads = 1;
-  obj.data[0].is_filter_ads = 1;
-  obj.data[0].is_ios_paid = 1;
-}
+obj.data[0].vip_split.is_filter_ads = 1;
+obj.data[0].is_filter_ads = 1;
+obj.data[0].is_ios_paid = 1;
+obj.data[0].tt_promotion = {};
 
 $done({
 	body: JSON.stringify(obj)
