cb8402d923c3bd2ddeaf588dd3fe5fbd5c1e0856
Bronco <heybronco@gmail.com>
2022-07-03 09:26:30 +0800

    Update users.js

--- users.js
+++ users.js
@@ -3,6 +3,7 @@ let obj = JSON.parse($response.body || '{}');
 obj.extra.adms_activity = {};
 obj.extra.adms = [];
 obj.extra.adms_user = [];
+obj.extra.nearby_dating = [];
 
 $done({
 	body: JSON.stringify(obj)
